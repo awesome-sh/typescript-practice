71de83ab16c6dcaba452358f07a8676061db8dfd
SungHwa <sihus@naver.com>
2020-11-23 11:55:00 +0900

    Typescript Project

---
+++ dist/hello.js
@@ -0,0 +1,4 @@
+import add from './util.js';
+var returnValue = add(3, 5);
+console.log(returnValue);
+//# sourceMappingURL=hello.js.map
---
+++ src/hello.ts
@@ -0,0 +1,3 @@
+import add from './util.js';
+const returnValue = add(3, 5);
+console.log( returnValue );
